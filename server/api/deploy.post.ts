258f2eca33b0de6accf5f3c16d885236aad6c1fe
Marat <marat.tmv1@gmail.com>
2025-08-22 17:27:46 +0700

    Validate body in each api handler

--- server/api/deploy.post.ts
+++ server/api/deploy.post.ts
@@ -1,7 +1,20 @@
+import * as v from "valibotx"
+
 import { deployBondingCurve } from "../lib/bondingCurve/deploy"
 
+const parseBody = v.flatErrorsParser(
+  v.object({
+    ownerAddress: v.string(),
+    metadata: v.object({
+      name: v.string(),
+      description: v.string(),
+      image: v.string(),
+    }),
+  }),
+)
+
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
+  const body = await readValidatedBody(event, parseBody)
   const { ownerAddress, metadata } = body
 
   const messages = await deployBondingCurve({
